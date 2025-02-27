32ad81a991bd91f895c808ae620f4d945738a75b
emmadorn <emmaedorn@gmail.com>
2025-02-27 16:51:51 -0600

    commit 2/27

--- scripts.js
+++ scripts.js
@@ -1,36 +1,60 @@
 
 const nos3Link = document.getElementById('nos3-link');
-console.log(`nos3link: ${nos3Link}`);
 nos3Link.addEventListener('click', (e) => {
-    console.log("THIS IS WORKING");
     document.getElementById('home').style.display = 'none';
     document.getElementById('nos3').style.display = 'block';
+    document.getElementById('installation').style.display = 'none';
     document.getElementById('cFS').style.display = 'none';
-    document.getElementById('tutorials').style.display = 'none';
+    document.getElementById('demos').style.display = 'none';
+    document.getElementById('other').style.display = 'none';
 });
 
+const installLink = document.getElementById('installation-link');
+installLink.addEventListener('click', (e) => {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('nos3').style.display = 'none';
+    document.getElementById('installation').style.display = 'block';
+    document.getElementById('cFS').style.display = 'none';
+    document.getElementById('demos').style.display = 'none';
+    document.getElementById('other').style.display = 'none';
+});
 
 const cfsLink = document.getElementById('cfs-link');
 cfsLink.addEventListener('click', (e) => {
     document.getElementById('home').style.display = 'none';
     document.getElementById('nos3').style.display = 'none';
+    document.getElementById('installation').style.display = 'none';
     document.getElementById('cFS').style.display = 'block';
-    document.getElementById('tutorials').style.display = 'none';
+    document.getElementById('demos').style.display = 'none';
+    document.getElementById('other').style.display = 'none';
 });
 
-
-const tutorialLink = document.getElementById('tutorials-link');
-tutorialLink.addEventListener('click', (e) => {
+const demoLink = document.getElementById('demos-link');
+demoLink.addEventListener('click', (e) => {
     document.getElementById('home').style.display = 'none';
     document.getElementById('nos3').style.display = 'none';
+    document.getElementById('installation').style.display = 'none';
     document.getElementById('cFS').style.display = 'none';
-    document.getElementById('tutorials').style.display = 'block';
+    document.getElementById('demos').style.display = 'block';
+    document.getElementById('other').style.display = 'none';
+});
+
+const otherLink = document.getElementById('other-link');
+otherLink.addEventListener('click', (e) => {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('nos3').style.display = 'none';
+    document.getElementById('installation').style.display = 'none';
+    document.getElementById('cFS').style.display = 'none';
+    document.getElementById('demos').style.display = 'none';
+    document.getElementById('other').style.display = 'block';
 });
 
 const homeLink = document.getElementById('home-link');
 homeLink.addEventListener('click', (e) => {
     document.getElementById('home').style.display = 'block';
     document.getElementById('nos3').style.display = 'none';
+    document.getElementById('installation').style.display = 'none';
     document.getElementById('cFS').style.display = 'none';
-    document.getElementById('tutorials').style.display = 'none';
+    document.getElementById('demos').style.display = 'none';
+    document.getElementById('other').style.display = 'none';
 });
